89c81c3dac6507365466abdba2ccdf211dfd2e30
Reedych <reedych@reedych.pw>
2017-03-18 22:39:07 +0700

    Более краткая формула
    Сам придумал :)

--- csc-java/week_02/booleans/Booleans.java
+++ csc-java/week_02/booleans/Booleans.java
@@ -2,6 +2,6 @@
 
 public class Booleans {
     public static boolean booleanExpression(boolean a, boolean b, boolean c, boolean d) {
-        return (a & b & !c & !d) | (a & !b & c & !d) | (a & !b & !c & d) | (!a & b & c & !d) | (!a & b & !c & d) | (!a & !b & c & d);
+        return (a ^ b) & (d ^ c) | (a ^ c) & (b ^ d);
     }
 }
